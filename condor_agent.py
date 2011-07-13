3ee1ff8a188cae582f7b5a2ae77c8e0504a9c5f2
Ian Chesal <ichesal@cyclecomputing.com>
2011-07-13 12:57:34 -0400

    Increase version to 1.16

--- condor_agent.py
+++ condor_agent.py
@@ -49,7 +49,7 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 ################################################################################
 # GLOBALS
 ################################################################################
-__version__ = "1.15"
+__version__ = "1.16"
 
 # URL Patterns for REST Calls
 #

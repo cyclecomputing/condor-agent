edfc0146b25d885f081ff92d12fe5e3444884c2a
Douglas Clayton <dclayton@cyclecomputing.com>
2013-02-21 12:22:16 -0500

    Disabled default of /etc/condor/config under Windows

--- condor_agent.py
+++ condor_agent.py
@@ -281,7 +281,8 @@ def main():
     # we need.
     if not os.environ.has_key("CONDOR_BIN_PATH"):
         os.environ["CONDOR_BIN_PATH"] = "/opt/condor/bin"
-    if not os.environ.has_key("CONDOR_CONFIG"):
+    if not os.environ.has_key("CONDOR_CONFIG") and os.name != 'nt':
+        # set this as a default, but not under Windows. (Under Windows Condor uses the registry.)
         os.environ["CONDOR_CONFIG"] = "/etc/condor/condor_config"
     if os.environ.has_key("CONDOR_BIN_PATH"):
         os.environ["PATH"] = os.environ["PATH"] + os.pathsep + os.environ["CONDOR_BIN_PATH"]

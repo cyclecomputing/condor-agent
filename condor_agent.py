992cf4d1d617eedb68a07e3dff55857f515a316c
Douglas Clayton <dclayton@cyclecomputing.com>
2013-03-12 19:27:44 -0400

    Version 1.22

--- condor_agent.py
+++ condor_agent.py
@@ -49,7 +49,7 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 ################################################################################
 # GLOBALS
 ################################################################################
-__version__ = "1.21"
+__version__ = "1.22"
 
 # URL Patterns for REST Calls
 #
